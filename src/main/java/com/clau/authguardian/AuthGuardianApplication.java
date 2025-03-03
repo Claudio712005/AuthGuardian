9b83f8ae45e6ab6fa1a2e3a85c3f9f686804423e
Claudio712005 <clausilvaaraujo11@gmail.com>
2025-03-03 13:06:43 -0300

    Iniciando projeto

---
+++ src/main/java/com/clau/authguardian/AuthGuardianApplication.java
@@ -0,0 +1,13 @@
+package com.clau.authguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthGuardianApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthGuardianApplication.class, args);
+  }
+
+}
